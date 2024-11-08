21209ae3a57f569d69892caeba20a26a3bc9b7e6
halyna-baklanova <novabalka@gmail.com>
2024-11-08 23:29:42 +0200

    fix model Movie

--- cinema/models.py
+++ cinema/models.py
@@ -4,3 +4,6 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     duration =models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
